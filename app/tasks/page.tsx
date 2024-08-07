a8e3416486e5dd05b91397ec9ec01148d08f2773
robw-raviga <robw@raviga.co.uk>
2024-08-07 22:46:34 +0100

    Start Tasks page

---
+++ app/tasks/page.tsx
@@ -0,0 +1,10 @@
+import { PageTitle } from "@/components/PageTitle";
+
+export default function Page() {
+
+    return (
+        <article>
+            <PageTitle>Your tasks</PageTitle>
+        </article>
+    )
+}

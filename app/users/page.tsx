c9a3c08b7f2dba13608fb810cf794553b3f0f59d
Rob Watson <source.pot.dev@gmail.com>
2024-08-11 14:44:21 +0100

    Change users page to a table

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -4,10 +4,23 @@ export default async function Page() {
   const users = await listUsers()
 
   return (
-    <ul>
-      {users.map((user, key) => (
-        <li key={key}><pre>{JSON.stringify(user, undefined, 2)}</pre></li>
-      ))}
-    </ul>
+    <table className={'w-full'}>
+      <thead>
+      <tr>
+        <th className={'p-1 text-left'}>ID</th>
+        <th className={'p-1 text-left'}>Email Address</th>
+        <th className={'p-1 text-left'}>Name</th>
+      </tr>
+      </thead>
+      <tbody>
+        {users.map((user, key) => (
+          <tr key={key} className={'hover:bg-slate-800'}>
+            <td className={'p-1 text-left'}>{ user.id }</td>
+            <td className={'p-1 text-left'}>{ user.email }</td>
+            <td className={'p-1 text-left'}>{ user.name}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   )
 }
